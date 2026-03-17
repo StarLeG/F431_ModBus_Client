dcb58314b5d9d231aa5d606ab3b7b0b527304344
Oleg Starinsky <oleg.starynski@gmail.com>
2026-03-17 08:04:17 +0300

    feat: добавлен модуль CRC для Modbus (crc_mb.h/c)
    Добавлены файлы crc_mb.h и crc_mb.c с реализацией расчета
    контрольной суммы CRC16 для Modbus RTU.

---
+++ Core/Inc/crc_mb.h
@@ -0,0 +1,6 @@
+#ifndef CRC_MB_H
+#define CRC_MB_H
+
+uint16_t crc_mb(uint8_t *data, uint16_t length);
+
+#endif // CRC_MB_H
---
+++ Core/Src/crc_mb.c
@@ -0,0 +1,21 @@
+#include "crc_mb.h"
+
+uint16_t crc_mb(uint8_t *data, uint16_t length)
+{
+    uint16_t crc = 0xFFFF;
+
+    for (uint16_t pos = 0; pos < length; pos++) {
+        crc ^= (uint16_t)data[pos];
+
+        for (int i = 8; i != 0; i--) {
+            if ((crc & 0x0001) != 0) {
+                crc >>= 1;
+                crc ^= 0xA001;
+            } else {
+                crc >>= 1;
+            }
+        }
+    }
+
+    return crc;
+}
